4f792f17835cd3745dfc914505723f0d5976c944
tomnoiprasit <tom.noiprasit@gmail.com>
2014-08-07 19:08:11 +1000

    Add Setup and ReleaseThemAll

--- D2DTutorial3/D2DTutorial3.h
+++ D2DTutorial3/D2DTutorial3.h
@@ -1,3 +1,15 @@
 #pragma once
-
 #include "resource.h"
+ID2D1Factory * pD2DFactory = NULL;
+ID2D1HwndRenderTarget* pD2DHRT = NULL;
+
+IWICImagingFactory* pWICImagingFactory = NULL;
+IWICBitmapDecoder* pWICBitmapDecoder = NULL;
+IWICFormatConverter* pWICFormatConverter = NULL;
+IWICBitmapFrameDecode* pBitmapFrameDecode = NULL;
+
+ID2D1Bitmap* pD2DBitmap = NULL;
+HRESULT hr;
+
+void Setup(HWND);
+void ReleaseThemAll();
